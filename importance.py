0af910a64fd6a2dddb05e90ff37ec81293d097a6
AidanTBroderick <aidantbroderick@gmail.com>
2025-02-22 17:19:36 -0500

    Add files via upload

---
+++ importance.py
@@ -0,0 +1,24 @@
+class Event:
+    def __init__(rating = None, duration, start, end, name, isMovable):
+        rating
+        self.duration
+        start = 0
+        end = 2400
+        name = ""
+        isMovable = False
+# class Preference:
+#     def __init__(rating, duration, start, end, name,split):
+#         rating = 5
+#         duration = 0
+#         start = 0
+#         end = 2400
+#         name = ""
+#         split = false
+class main:
+   App1 = Appointment(5,120,1200,1400,"Econ")
+   App2 = Appointment(5,90,1600,1750,"English")
+   App3 = Appointment(5,60,900,1000,"CS club")
+   App4 = Appointment(5,50,1100,1150,"Math")
+   Pref1 = Preference(3,20,1100)
+
+
