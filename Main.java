76a37e3aeff9c0b5dd16f24fbab4aa7578b14516
EJMcClanahan <mcclane1@tcnj.edu>
2025-02-22 19:48:40 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -45,6 +45,19 @@ public class Main{
             }
 
             if (chooseChar == 1 || chooseChar == 2 || chooseChar == 3) {
+
+                if(chooseChar == 1){
+                    characteristic = "Balanced";
+                }
+        
+                else if(chooseChar == 2){
+                    characteristic = "Fronthand";
+                }
+        
+                else if(chooseChar == 3){
+                    characteristic = "Fronthand";
+                }
+
                 break; 
             } 
             
@@ -54,6 +67,95 @@ public class Main{
             }
         }
 
+        System.out.println("Nice! Now let's start with your important events.");
+        System.out.println("How many important events do you have today? (Type the number of events)");
+        int importantEvents = scan.nextInt();
+        scan.nextLine(); 
+        String[][] iEvents = new String[importantEvents][2];
+
+        System.out.println("How many extracurriculars are you doing today? (Type the number of events)");
+        int extraEvents = scan.nextInt();
+        scan.nextLine(); 
+        String[][] exEvents = new String[extraEvents][2];
+
+        System.out.println("What fun events are you doing today such as hanging out with friends? (Type the number)");
+        int funEvents = scan.nextInt();
+        scan.nextLine(); 
+        String[][] fEvents = new String[funEvents][2];
+
+        System.out.println("Lastly, what is the day today?");
+        String weekDay = scan.nextLine();
+
+        // important events
+        System.out.println("For important events such as classes, meetings, etc., what are the events called?");
+        for (int i = 0; i < iEvents.length; i++) {
+            System.out.print("Event " + (i + 1) + ": ");
+            iEvents[i][0] = scan.nextLine();
+        }
+
+        System.out.println("Now input the time for each event in minutes (e.g., 80 minutes is one hour and twenty minutes)");
+        for (int i = 0; i < iEvents.length; i++) {
+            System.out.print("Time for " + iEvents[i][0] + ": ");
+            iEvents[i][1] = scan.nextLine();
+        }
+
+        System.out.println("Now enter the time this will be at. (Enter a number such as 5 to indictate the hour of day)");
+        for (int i = 0; i < iEvents.length; i++) {
+            System.out.print("Time of day for " + iEvents[i][0] + ": ");
+            iEvents[i][2] = scan.nextLine();
+        }
+
+        // extracul
+        System.out.println("Now, enter the names of your extracurricular events:");
+        for (int i = 0; i < exEvents.length; i++) {
+            System.out.print("Extracurricular " + (i + 1) + ": ");
+            exEvents[i][0] = scan.nextLine();
+        }
+
+        System.out.println("Enter the duration (in minutes) for each extracurricular event:");
+        for (int i = 0; i < exEvents.length; i++) {
+            System.out.print("Time for " + exEvents[i][0] + ": ");
+            exEvents[i][1] = scan.nextLine();
+        }
+
+        System.out.println("Now enter the time this will be at. (Enter a number such as 5 to indictate the hour of day)");
+        for (int i = 0; i < iEvents.length; i++) {
+            System.out.print("Time of day for " + exEvents[i][0] + ": ");
+            iEvents[i][2] = scan.nextLine();
+        }
+
+        // fun events
+        System.out.println("Now, enter the names of your fun events:");
+        for (int i = 0; i < fEvents.length; i++) {
+            System.out.print("Fun Event " + (i + 1) + ": ");
+            fEvents[i][0] = scan.nextLine();
+        }
+
+        System.out.println("Enter the duration (in minutes) for each fun event:");
+        for (int i = 0; i < fEvents.length; i++) {
+            System.out.print("Time for " + fEvents[i][0] + ": ");
+            fEvents[i][1] = scan.nextLine();
+        }
+
+        System.out.println("Now enter the time this will be at. (Enter a number such as 5 to indictate the hour of day)");
+        for (int i = 0; i < iEvents.length; i++) {
+            System.out.print("Time of day for " + fEvents[i][1] + ": ");
+            iEvents[i][2] = scan.nextLine();
+        }
+
+        System.out.println("Schedule input complete!");
+        Thread.sleep(2000);
+        System.out.println("Let's see how your schedule looks!");
+
+
+    }
+
+    String TCNJCourseReader(String lol){
+        return lol;
+    }
+
+    void PersonalityTest(){
+        System.out.println("Do you prefer");
     }
 }
 
